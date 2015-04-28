699cfddb9eb875cd6ebbe1f17024284bd2fcdeb3
Jean Cruypenynck <filaton@me.com>
2015-04-28 11:22:57 +0200

    Updates the structure of the program First totally compilable version of the project. Stops using "MSCallback.h/.c" as it seems that portaudio doesn't accept external callback functions. Dummy function "conv" and commented functions in "binauralize.h/.c" are meant to make the program compilable.

--- binauralize.h
+++ binauralize.h
@@ -3,9 +3,12 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <sndfile.h>
 #include "portaudio.h"
 #include "MSCallback.h"
 
-float Binauralize( float input );
+//float			Binauralize		( float input, int index );
+//SNDFILE*		read_wave		( char* , SF_INFO* );
+float			conv			( float, int );
 
 #endif
--- main.c
+++ main.c
@@ -4,13 +4,50 @@
 #include "binauralize.h"
 #include "MSCallback.h"
 
-#define SAMPLE_RATE         (44100)
+#define SAMPLE_RATE         (48000)
 #define PA_SAMPLE_TYPE      paInt24
-#define FRAMES_PER_BUFFER   (64)
+#define FRAMES_PER_BUFFER   (256)
 
 typedef float SAMPLE;
 
-static int gNumNoInputs = 0;
+static int MSCallback( const void *inputBuffer, void *outputBuffer,
+                         unsigned long framesPerBuffer,
+                         const PaStreamCallbackTimeInfo* timeInfo,
+                         PaStreamCallbackFlags statusFlags,
+                         void *userData );
+
+
+
+static int MSCallback( const void *inputBuffer, void *outputBuffer,
+                         unsigned long framesPerBuffer,
+                         const PaStreamCallbackTimeInfo* timeInfo,
+                         PaStreamCallbackFlags statusFlags,
+                         void *userData )
+{
+    SAMPLE *out = (SAMPLE*)outputBuffer;
+    const SAMPLE *in = (const SAMPLE*)inputBuffer;
+    unsigned int i;
+    (void) timeInfo; /* Prevent unused variable warnings. */
+    (void) statusFlags;
+    (void) userData;
+    int IR_LL, IR_LR, IR_LC, IR_LLs, IR_LRs, IR_RL, IR_RR, IR_RC, IR_RLS, IR_RRS;
+    for( i=0; i<framesPerBuffer; i++ )
+    {
+        out[i*2]   =    conv(in[i*5],IR_LL) +
+                        conv(in[i*5],IR_LR) +
+                        conv(in[i*5],IR_LC) +
+                        conv(in[i*5],IR_LLs)+
+                        conv(in[i*5],IR_LRs);
+
+        out[i*2+1] =    conv(in[i*5],IR_RL) +
+                        conv(in[i*5],IR_RR) +
+                        conv(in[i*5],IR_RC) +
+                        conv(in[i*5],IR_RLs)+
+                        conv(in[i*5],IR_RRs);
+
+    } 
+    return paContinue;
+}
 
 /*******************************************************************/
 int main(void);
@@ -28,7 +65,7 @@ int main(void)
       fprintf(stderr,"Error: No default input device.\n");
       goto error;
     }
-    inputParameters.channelCount = 5;       /* stereo input */
+    inputParameters.channelCount = 5;       /* multichannel 5.1 input */
     inputParameters.sampleFormat = PA_SAMPLE_TYPE;
     inputParameters.suggestedLatency = Pa_GetDeviceInfo( inputParameters.device )->defaultLowInputLatency;
     inputParameters.hostApiSpecificStreamInfo = NULL;
@@ -62,7 +99,7 @@ int main(void)
     err = Pa_CloseStream( stream );
     if( err != paNoError ) goto error;
 
-    printf("Finished. gNumNoInputs = %d\n", gNumNoInputs );
+    printf("Streams closed.\n");
     Pa_Terminate();
     return 0;
 
